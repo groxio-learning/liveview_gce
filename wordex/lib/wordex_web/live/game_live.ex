c6dfd7b8c82b1bd2c0d5dc76bdd025d15fb1f576
Logan Siefker <lsiefker@claruscredits.com>
2023-05-18 14:39:08 -0400

    TEAM WORD

--- wordex/lib/wordex_web/live/game_live.ex
+++ wordex/lib/wordex_web/live/game_live.ex
@@ -7,31 +7,6 @@ defmodule WordexWeb.GameLive do
     {:ok, in_progress_game(socket)}
   end
 
-  def render(assigns) do
-    ~H"""
-    hello, world
-
-    <pre>
-      <%= inspect @game, pretty: true %>
-    </pre>
-    <br/>
-    <br/>
-    <hr/>
-    <br/>
-    <br/>
-    <pre>
-      <%= inspect @result, pretty: true %>
-    </pre>
-
-    <div class="grid grid-cols-10 gap-3 text-center font-bold">
-      <%= for {letter, color} <- @result.keyboard do %>
-        <KeyboardKey.key letter={letter} color={color} />
-      <% end %>
-    </div>
-
-    """
-  end
-
   defp in_progress_game(socket) do
     game =
       Game.new()
@@ -41,5 +16,64 @@ defmodule WordexWeb.GameLive do
     result = Game.render(game)
 
     assign(socket, game: game, result: result)
+  end
+
+  def render(assigns) do
+    ~H"""
+    <.word_grid scores={@game.scores}/>
+
+    <pre>
+    <%= inspect @game, pretty: true %>
+    </pre>
+
+    <div class="grid grid-cols-10 gap-3 text-center font-bold">
+      <%= for {letter, color} <- @result.keyboard do %>
+        <KeyboardKey.key letter={letter} color={color} />
+      <% end %>
+    </div>
+    """
+  end
+
+  attr :scores, :list
+  def word_grid(assigns) do
+    ~H"""
+    <%= for index <- 0..5 do %>
+      <.word score={@scores |> Enum.at(index)}/>
+    <% end %>
+    """
+  end
+
+  attr :score, :list
+  def word(%{score: nil} = assigns) do
+    ~H"""
+    <div class="grid grid-cols-5 gap-4 text-center font-bold">
+      <.word_letter letter="" color=""/>
+      <.word_letter letter="" color=""/>
+      <.word_letter letter="" color=""/>
+      <.word_letter letter="" color=""/>
+      <.word_letter letter="" color=""/>
+    </div>
+    """
+  end
+
+  def word(assigns) do
+    ~H"""
+    <div class="grid grid-cols-5 gap-4 text-center font-bold">
+      <%= for {letter, color} <- @score do %>
+        <.word_letter letter={letter} color={color}/>
+      <% end %>
+    </div>
+    """
+  end
+
+  @doc """
+  NOT MY PROBLEM!
+  """
+  attr :letter, :string
+  attr :color, :atom
+  def word_letter(assigns) do
+    ~H"""
+    <div class="border h-8 pt-2 pb-2 rounded"><%= @letter %> <%= @color %></div>
+    """
   end
 end
